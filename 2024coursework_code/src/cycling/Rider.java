190b55473ca75b5f996803dbd437042272baa8aa
annabelle-josie <ajroxford@gmail.com>
2024-03-11 13:53:28 +0000

    Rider updated and Team added. Team has an attribute _riders_ which is an Array of Riders in that team. This is the connection between them

---
+++ 2024coursework_code/src/cycling/Rider.java
@@ -0,0 +1,58 @@
+package cycling;
+
+/**
+ * Rider Class
+ * <p>
+ * Allows riders to be created, currently accepts name, yearOfBirth and sets the ID
+ * <p>
+ * Just getters and setters (basically)
+ */
+
+public class Rider{
+    //TODO: Is it best programming practice to declare as a group or individually?
+    private int riderID, yearOfBirth;
+    private static int nextID; //Static as independent of instances
+    private String name;
+    //TODO: Some way of counting their point totals
+
+    /**
+     * Constructor class for rider
+     * Creates new Rider with unique ID
+     */
+    public Rider(){
+        //ID will incriment each time. If a rider is deleted then there will just be a gap in the numbers
+        this.riderID = nextID;
+        nextID++;
+    }
+
+    /**
+     * Allows Riders's details to be added to the Rider
+     * @param name          The name of the rider
+     * @param yearOfBirth   The birth year of the rider
+     */
+    public void fillDetails(String name, int yearOfBirth){
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    /**
+     * @return The unique ID of the rider
+     */
+    public int getRiderID(){
+        return riderID;
+    }
+
+    /**
+     * @return The rider's name
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * @return The birth year of the rider
+     */
+    public int getYearOfBirth(){
+        return yearOfBirth;
+    }
+}
---
+++ 2024coursework_code/src/cycling/Team.java
@@ -0,0 +1,91 @@
+package cycling;
+import java.util.ArrayList;
+
+/**
+ * Team Class
+ * <p>
+ * Allows Teams to be created with unique IDs, names and descriptions
+ * <p>
+ * Also an ArrayList containing the riders who are part of the team
+ */
+
+public class Team{
+    private int teamID;
+    private int nextID;
+    private ArrayList<Rider> riders = new ArrayList<Rider>();
+    private String name, description;
+
+    /**
+     * Constructor class for rider
+     * <p>
+     * Creates new Team with unique ID
+     */
+    public Team(){
+        this.teamID = nextID;
+        nextID++; 
+    }
+
+    /**
+     * Allows team details to be added to the team
+     * @param name          Name of the Team
+     * @param description   Description of the Team
+     */
+    public void fillDetails(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    /**
+     * @return Unique ID of the team
+     */
+    public int getTeamID(){
+        return teamID;
+    }
+
+    /**
+     * @return Name of the team
+     */
+    public String getName(){
+        return name;
+    } 
+
+    /**
+     * @return Description of the team
+     */
+    public String getDescription(){
+        return description;
+    } 
+
+    /**
+     * Adds a rider to the team
+     * <p>
+     * There is a very real chance this doesn't work
+     * TODO: Test and debate changing to arraylist of integers (RiderIDs)
+     * <p>
+     * Currently Rider is inputted, this should be changed to ID when brain works
+     * <p>
+     * TODO: Change to accept these
+     * @param riderID                       The ID of the rider to be added
+     * @throws IDNotRecognisedException     If a rider with that ID does not exist
+     */
+    public void addRider(Rider newRider){
+        //riderID
+        riders.add(newRider);
+    }
+    
+    /**
+     * Returns a list the Riders IDs
+     * <p>
+     * This will simplify if addRider() is changed
+     * TODO: Test and debate changing to arraylist of integers (RiderIDs) 
+     * @return Array of RiderIDs
+     */
+    public int[] getRiderIDList(){
+        int[] riderIDs = new int[riders.size()];
+        int count = 0;
+        for (Rider rider : riders) {
+            riderIDs[count] = rider.getRiderID();
+        }
+        return riderIDs;
+    }
+}
